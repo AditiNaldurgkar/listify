ed0841f0c9e79526d322e25d9a76cc3077df5946
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-11-17 23:06:38 +0530

    basic layout and img done

--- src/home/home.js
+++ src/home/home.js
@@ -1,7 +1,11 @@
 import "./home.css"
+import icon1 from "./icon1.png"
 function Home(){
-    return <div>
-        hie
+    return <div className="homebody">
+        <img src={icon1} className="img1"></img>
+        <div className="hometext">
+        <h1 className="main-heading">Listify</h1>
+        </div>
     </div>
 }
 export default Home;
