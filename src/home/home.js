40c0a0395ad22e8790026b8b1aa764ce52a97450
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-11-18 22:20:35 +0530

    major changes in css

--- src/home/home.js
+++ src/home/home.js
@@ -3,9 +3,12 @@ import icon1 from "./icon1.png"
 function Home(){
     return <div className="homebody">
         <img src={icon1} className="img1"></img>
-        <div>
+        <div className="bodyhome">
         <h1 className="main-heading">Listify</h1>
-        heloieifepojowjwoegv
+            <p>Hello everyone!!!!</p>
+            <span> Introducing Listify ! An app helping to turn chaos into checkmarks!</span><br/>
+            <button type="button" className="addbtn"> Add Note</button>
+            <button type="button" className="showbtn">Show Note</button>
         </div>
     </div>
 }
