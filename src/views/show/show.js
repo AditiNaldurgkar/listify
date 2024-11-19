920baa15ef8ebbf7dbd2ed8d5a3ca492e6918d75
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-11-19 18:43:58 +0530

    show task card made

--- src/views/show/show.js
+++ src/views/show/show.js
@@ -1,14 +1,26 @@
 import "./show.css"
 const tasks =[
-    {title :"finish revision of maths",type:"study"},
+    {title :"Finish revision of maths",type:"study"},
     {
-        title:"buy fresj veggies",type:"daily work"
+        title:"buy fresh veggies",type:"daily work"
 }]
-
-function Shownote(){
-    return <div className="bodyshow">
-         <h1 className="taglineshow">Here are your tasks for the day !</h1>
-
-    </div>
+function Displaynotes() {
+    return tasks.map((task, index) => (
+        <div key={index} className="taskcard">
+            <h3>{task.title}</h3>
+            <i>{task.type}</i>
+            <button></button>
+        </div>
+    ));
 }
+
+function Shownote() {
+    return (
+        <div className="bodyshow">
+            <h1 className="taglineshow">Here are your tasks for the day!</h1>
+            <Displaynotes />
+        </div>
+    );
+}
+
 export default Shownote;
