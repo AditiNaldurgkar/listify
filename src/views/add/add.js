c4c0b2806a3a14a82217ebefceec744c8f58a657
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-11-19 18:14:45 +0530

    started working on show page

--- src/views/add/add.js
+++ src/views/add/add.js
@@ -1,6 +1,7 @@
+
 function Addnote(){
     return <div>
-        <h1>addnote</h1>
+       
     </div>
 }
 export default Addnote;
--- src/views/show/show.js
+++ src/views/show/show.js
@@ -1,6 +1,14 @@
+import "./show.css"
+const tasks =[
+    {title :"finish revision of maths",type:"study"},
+    {
+        title:"buy fresj veggies",type:"daily work"
+}]
+
 function Shownote(){
-    return <div>
-        show
+    return <div className="bodyshow">
+         <h1 className="taglineshow">Here are your tasks for the day !</h1>
+
     </div>
 }
 export default Shownote;
