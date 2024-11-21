6c005dd1a58ca8f623c62a688576be7e65330d68
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-11-21 23:08:49 +0530

    form created

--- src/views/add/add.js
+++ src/views/add/add.js
@@ -3,6 +3,17 @@ function Addnote(){
     return <div className="addcontainer">
        <h2 className="addtag">Have too many tasks to do ?😫</h2>
        <h2 className="subtag">No worries add them here 👇</h2>
+       <div className="form">
+       <label>Task title :</label>
+       <input type="text"></input>
+       <br/>
+       <label>Task description :</label>
+       <input type="text"></input>
+       <button type="button" className="addtaskbtn">Add Task</button>
+
+       </div>
+       
+
     </div>
 }
 export default Addnote;
