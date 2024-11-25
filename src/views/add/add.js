a8c714c684044c69717fbc0da5b5d4e07c983f72
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-11-25 09:53:30 +0530

    done

--- src/views/add/add.js
+++ src/views/add/add.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./add.css"
 import toast ,{Toaster}from "react-hot-toast";
+import home from "./../../house.png"
+import { Link } from "react-router-dom";
+import show from "./../../eye.png"
 
 function Addnote(){
     const [title,setTitle] = useState("");
@@ -8,12 +11,14 @@ function Addnote(){
     return <div className="addcontainer">
        <h2 className="addtag">Have too many tasks to do ?😫</h2>
        <h2 className="subtag">No worries add them here 👇</h2>
+       <Link to="/"><img src={home} alt="homebtn" className="homebtn"></img></Link>
+       <Link to="/show"><img src={show} className="shownotes"></img></Link>
        <div className="form">
        <label>Task title :</label>
-       <input type="text" onChange={(e)=>setTitle(e.target.value)}></input>
+       <input type="text" value={title}onChange={(e)=>setTitle(e.target.value)}></input>
        <br/>
        <label>Task description :</label>
-       <input type="text" onChange={(e)=>{setDesp(e.target.value)}}></input>
+       <input type="text" value={desp} onChange={(e)=>{setDesp(e.target.value)}}></input>
        <button type="button" className="addtaskbtn" onClick={addTask}>Add Task</button>
 <Toaster/>
        </div>
@@ -24,6 +29,9 @@ function Addnote(){
         notes.push(noteobj);
         localStorage.setItem( "tasks",JSON.stringify(notes));
         toast.success("Task added successfully");
+        setTitle("");
+        setDesp("");
+        
     }
 }
 
--- src/views/show/show.js
+++ src/views/show/show.js
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import "./show.css"
 import toast,{Toaster} from "react-hot-toast";
-
+import home from "./../../house.png"
+import { useEffect } from "react";
 function Displaynotes() {
     const tasks = JSON.parse(localStorage.getItem("tasks"));   
-
+    useEffect(() => {
+        toast.success("Task loaded successfully");
+    }, []); 
     return tasks.map((task, index) => (
         <div key={index} className="taskcard">
             <h3>{task.title}</h3>
@@ -16,19 +20,22 @@ function Displaynotes() {
     
   
 }
-function deletenote(index){
-    const tasks = JSON.parse(localStorage.getItem("tasks"))||[];   
-    tasks.splice(index,1);
-    localStorage.setItem("tasks",JSON.stringify(tasks));
-    toast.success("deleted task successfully");
-    window.location.reload();
-    }
+function deletenote(index) {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    setTimeout(() => {
+        window.location.reload();
+    });
+    toast.success("Deleted task successfully");
+}
+
 
 
 function Shownote() {
-    toast.success("task loaded successfully");
     return (
         <div className="bodyshow">
+            <Link to="/"><img src={home} alt="homebtn" className="homebtn"></img></Link>
             <h1 className="taglineshow">Here are your tasks for the day!</h1>
             <Displaynotes />
             <Toaster/>
